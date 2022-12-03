bd30aac8a7dc9283adcb450e64a875506abc3956
Alan Baker <abaker@signifyhealth.com>
2022-12-03 00:41:28 -0600

    Update Day03B.cs

--- Day03/Day03B.cs
+++ Day03/Day03B.cs
@@ -2,7 +2,7 @@ namespace aoc_2022_csharp.Day03;
 
 public class Day03
 {
-    private const string PriorityValues = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string PriorityValues = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public static int ExecuteA(IEnumerable<string> file)
     { 
@@ -12,7 +12,8 @@ public class Day03
                     A = x.Substring(0, x.Length / 2),
                     B = x.Substring(x.Length / 2, x.Length - (x.Length / 2))
                 })
-            .Select(x => PriorityValues.IndexOf(x.A.Intersect(x.B).First()) + 1)
+            .Select(x => x.A.Intersect(x.B).Single())
+            .Select(x => PriorityValues.IndexOf(x))
             .Sum();
        
         return sum;
@@ -20,7 +21,8 @@ public class Day03
     public static int ExecuteB(IEnumerable<string> file)
     {
         var sum = file.Chunk(3)
-            .Select(x => PriorityValues.IndexOf(x[0].Intersect(x[1]).Intersect(x[2]).First()) + 1)
+            .Select(x => x[0].Intersect(x[1]).Intersect(x[2]).Single())
+            .Select(x => PriorityValues.IndexOf(x))
             .Sum();
 
         return sum;
